09d0afa9caacf94ce6fb0e3fccafdd9af145e4da
resided <matveyshakhmaev11@gmail.com>
2026-07-23 11:07:50 +0500

    commit

---
+++ complexNumbers.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+export class Complex{
+    constructor(re = 0, im = 0){
+        this.re = re;
+        this.im = im;
+    }
+
+    static add(z1 = new Complex(0, 0), z2 = new Complex(0, 0)){
+        return new Complex(z1.re + z2.re, z1.im + z2.im);
+    }
+
+    static sub(z1 = new Complex(0, 0), z2 = new Complex(0, 0)){
+        return new Complex(z1.re - z2.re, z1.im - z2.im);
+    }
+
+    static square(z = new Complex(0, 0)){
+        return new Complex(z.re * z.re - z.im * z.im, 2 * z.re * z.im);
+    }
+
+    static mandelbrot_set_check(c = new Complex(0, 0), iterations = 0){
+        let z = new Complex(0, 0);
+
+        var i = 0
+        while(i < iterations && z.sqrMagnitude() <= 4){
+            z = z.square().add(c);
+            i+=1
+        }
+        if(i == iterations){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    sqrMagnitude(){
+        return this.re * this.re + this.im * this.im;
+    }
+
+    sub(z = new Complex(0, 0)){
+        return new Complex(this.re - z.re, this.im - z.im);
+    }
+
+    add(z = new Complex(0, 0)){
+        return new Complex(this.re + z.re , this.im + z.im);
+    }
+
+    square(){
+        return new Complex(this.re * this.re - this.im * this.im, 2 * this.re * this.im);
+    }
+
+}
---
+++ script.js
@@ -0,0 +1,55 @@
+// @ts-check
+
+import { Complex } from './complexNumbers.js';
+
+class vec2{
+    constructor(x = 0, y = 0){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+/** @type {HTMLCanvasElement} */
+// @ts-ignore
+const canvas = document.getElementById("fractal");
+
+const ctx = canvas?.getContext("2d");
+
+const w = canvas.width;
+const h = canvas.height;
+
+// @ts-ignore
+const imageData = ctx.createImageData(w, h);
+const data = imageData.data
+
+const iters = 100;
+const currentPixelComplex = new Complex(0, 0);
+
+const scaleFactor = 0.005;
+
+for(var i = 0; i < h; i++){
+    currentPixelComplex.im = (i - h/2) * scaleFactor;
+    for(var j = 0; j < w; j++){
+        currentPixelComplex.re = (j - w/2) * scaleFactor;
+
+        const pixelIndex = (i * w + j) * 4;
+
+        if(Complex.mandelbrot_set_check(currentPixelComplex, iters) == true){
+            data[pixelIndex] = 0;
+            data[pixelIndex + 1] = 0;
+            data[pixelIndex + 2] = 0;
+        }
+        else{
+            data[pixelIndex] = 255;
+            data[pixelIndex + 1] = 255;
+            data[pixelIndex + 2] = 255;
+        }
+        data[pixelIndex + 3] = 255
+    }
+}
+
+ctx?.putImageData(imageData, 0, 0);
+
+function pixelToComplex(x = 0, y = 0){
+    return new Complex(x - w/2, y - h/2);
+}
